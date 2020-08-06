d5dc780f073725acda2229dfdcc901584357ee76
Josué Heringer <josuedelgadoheringer98@gmail.com>
2020-08-06 10:17:25 -0300

    Updates

---
+++ precache-manifest.c7859f23519c1946aae12eaad257c5a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/bootcamp-appmasters/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "01fc6f08fbf807e6de70",
+    "url": "/bootcamp-appmasters/static/js/runtime~main.01fc6f08.js"
+  },
+  {
+    "revision": "4beb72f006510d6fb517",
+    "url": "/bootcamp-appmasters/static/js/main.4beb72f0.chunk.js"
+  },
+  {
+    "revision": "e851e6f54ae93aa2bbf6",
+    "url": "/bootcamp-appmasters/static/js/2.e851e6f5.chunk.js"
+  },
+  {
+    "revision": "4beb72f006510d6fb517",
+    "url": "/bootcamp-appmasters/static/css/main.091bfce8.chunk.css"
+  },
+  {
+    "revision": "e851e6f54ae93aa2bbf6",
+    "url": "/bootcamp-appmasters/static/css/2.16ea0fda.chunk.css"
+  },
+  {
+    "revision": "1cbdc6dc55f087208b74b0b5be145b46",
+    "url": "/bootcamp-appmasters/index.html"
+  }
+];
